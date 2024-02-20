8337064f8f2d610edb8e2db43cb666623b1476fb
YurkevichVA <bekker.volodymyr.yu@gmail.com>
2024-02-20 16:38:18 +0200

    Environment base

---
+++ Assets/Scripts/Environment/Cover.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cover : MonoBehaviour, ICover
+{
+    public bool CanCover()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void ICover.Cover()
+    {
+        throw new System.NotImplementedException();
+    }
+}
