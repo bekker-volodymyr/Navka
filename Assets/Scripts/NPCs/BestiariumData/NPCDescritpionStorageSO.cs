d2eb0f7458745ce871655cf6197d353e8b7151c6
Infinital-X <4h3art1ess@gmail.com>
2024-04-09 23:51:03 +0300

    BestiariumUpdate2

---
+++ Assets/Scripts/NPCs/BestiariumData/NPCDescritpionStorageSO.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCDescritpionStorageSO
+{
+    List<NPCDescriptionSO> NPCDescriptions;
+}
---
+++ Assets/Scripts/UI/BestiariumManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BestiariumManager
+{
+    [SerializeField] NPCDescriptionStorageSO NPCStorage;
+    [SerializeField] GameObject buttonsHumanNPC;
+    [SerializeField] GameObject buttonsUnholyEntityNPC;
+    [SerializeField] GameObject buttonsAnimalNPC;
+    [SerializeField] GameObject buttonsGodNPC;
+    void Initialize()
+    {
+
+    }
+    void SwitchButtonList(Enums.NPCTypes type)
+    {
+
+    }
+    void SwitchNPC(string Name)
+    {
+        
+    }
+}
