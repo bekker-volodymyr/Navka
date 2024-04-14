73b0df90ebd391cc6eb9b0e392a20ffa48c29fd4
YurkevichVA <bekker.volodymyr.yu@gmail.com>
2024-04-14 19:11:18 +0300

    fixes before demo

--- Assets/Scripts/UI/PlayerMenu/Items/ItemsPageButton.cs
+++ Assets/Scripts/UI/PlayerMenu/Items/ItemsPageButton.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ItemsPageButton : MonoBehaviour
@@ -7,6 +8,13 @@ public class ItemsPageButton : MonoBehaviour
     public ItemsPageManager pageManager;
 
     public ItemSO item;
+
+    public void InitButton(ItemSO item)
+    {
+        this.item = item;
+        TextMeshProUGUI text = GetComponentInChildren<TextMeshProUGUI>();
+        text.SetText(item.name);
+    }
 
     public void OnClick()
     {
--- Assets/Scripts/UI/PlayerMenu/Items/ItemsPageManager.cs
+++ Assets/Scripts/UI/PlayerMenu/Items/ItemsPageManager.cs
@@ -19,11 +19,15 @@ public class ItemsPageManager: MonoBehaviour
     [SerializeField] private Image Item_Image;
     [SerializeField] private TextMeshProUGUI Item_Name;
     [SerializeField] private TextMeshProUGUI Item_Description;
-    [SerializeField] private TMP_SubMeshUI Item_Recepies;
-    [SerializeField] private TMP_SubMeshUI Item_Sources;
-    [SerializeField] private TMP_SubMeshUI Item_Type;
-    [SerializeField] private TMP_SubMeshUI Item_Effect;
+    [SerializeField] private TextMeshProUGUI Item_Recepies;
+    [SerializeField] private TextMeshProUGUI Item_Sources;
+    [SerializeField] private TextMeshProUGUI Item_Type;
+    [SerializeField] private TextMeshProUGUI Item_Effect;
 
+    private void Start()
+    {
+        Initialize();
+    }
 
     public void Initialize()
     {
@@ -37,18 +41,22 @@ public class ItemsPageManager: MonoBehaviour
                 case Enums.ItemType.Food:
                     newButton = Instantiate(buttonPrefab);
                     newButton.transform.SetParent(buttonsFood.transform, false);
+                    newButton.GetComponent<ItemsPageButton>().InitButton(item);
                     break;
                 case Enums.ItemType.Amulet:
                     newButton = Instantiate(buttonPrefab);
                     newButton.transform.SetParent(buttonsAmulets.transform, false);
+                    newButton.GetComponent<ItemsPageButton>().InitButton(item);
                     break;
                 case Enums.ItemType.Potion:
                     newButton = Instantiate(buttonPrefab);
                     newButton.transform.SetParent(buttonsPotions.transform, false);
+                    newButton.GetComponent<ItemsPageButton>().InitButton(item);
                     break;
                 case Enums.ItemType.Component:
                     newButton = Instantiate(buttonPrefab);
                     newButton.transform.SetParent(buttonsComponents.transform, false);
+                    newButton.GetComponent<ItemsPageButton>().InitButton(item);
                     break;
                 default:
                     throw new Exception("uknown type exception");
@@ -58,21 +66,21 @@ public class ItemsPageManager: MonoBehaviour
     }
 
 
-    public void SwitchButtonList(Enums.ItemType type)
+    public void SwitchButtonList(int type)
     {
-        if(type == Enums.ItemType.Amulet)
+        if(type == 1)
         {
             buttonsAmulets.SetActive(true);
         }
-        else if (type == Enums.ItemType.Component)
+        else if (type == 2)
         {
             buttonsComponents.SetActive(true);
         }
-        else if (type == Enums.ItemType.Food)
+        else if (type == 3)
         {
             buttonsFood.SetActive(true);
         }
-        else if (type == Enums.ItemType.Potion)
+        else if (type == 4)
         {
             buttonsPotions.SetActive(true);
         }
@@ -84,8 +92,8 @@ public class ItemsPageManager: MonoBehaviour
         Item_Name.SetText(item.Name);
         Item_Description.SetText(item.Lore); //?
         //Item_Recepies.textComponent.SetText(item.); -- missing
-        Item_Sources.textComponent.SetText(item.Sources.ToString());
-        Item_Type.textComponent.SetText(item.Type.ToString());
-        Item_Effect.textComponent.SetText(item.Effect);
+        Item_Sources.SetText(item.Sources.ToString());
+        Item_Type.SetText(item.Type.ToString());
+        Item_Effect.SetText(item.Effect);
     }
 }
