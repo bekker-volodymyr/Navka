0dd49d94ab570e21153f17a6cc84af2241735aca
MassCon <53021665+MassCon@users.noreply.github.com>
2024-05-09 20:36:33 +0200

    World Generator v2
    objects generations through layers

---
+++ Assets/Scripts/WorldGenerator/ItemGenerator V2/ObjectGeneration.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+public class ObjectGenerator : MonoBehaviour
+{
+
+    [SerializeField] private GameObject objectPrefab;
+    [SerializeField] private int Density = 10;
+    public Vector2 spawnAreaSize = new Vector2(10f, 10f);
+    public LayerMask collisionLayer; // Define the layer mask for collision checking
+
+    void Start()
+    {
+        GenerateObjects();
+    }
+
+    void GenerateObjects()
+    {
+        for (int i = 0; i < Density; i++)
+        {
+            Vector2 randomPosition = GetRandomPosition();
+
+            // Check for collision
+            Collider2D[] colliders = Physics2D.OverlapBoxAll(randomPosition, objectPrefab.transform.localScale, 0, collisionLayer);
+            if (colliders.Length == 0) // If no collision detected, instantiate the object
+            {
+                Instantiate(objectPrefab, randomPosition, Quaternion.identity, transform);
+            }
+        }
+    }
+
+    Vector2 GetRandomPosition()
+    {
+        return transform.position + new Vector3(UnityEngine.Random.Range(-spawnAreaSize.x / 2, spawnAreaSize.x / 2),
+                                                 UnityEngine.Random.Range(-spawnAreaSize.y / 2, spawnAreaSize.y / 2),
+                                                 0);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(spawnAreaSize.x, spawnAreaSize.y, 1));
+    }
+}
