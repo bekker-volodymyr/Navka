892df7e8a3cd47b0185f74f46939700f13648f3e
MassCon <53021665+MassCon@users.noreply.github.com>
2024-06-05 19:11:06 +0200

    Multiplayer final fix

--- Assets/Scripts/UI/PlayerMenu/MainMenu/WorldManager.cs
+++ Assets/Scripts/UI/PlayerMenu/MainMenu/WorldManager.cs
@@ -14,4 +14,9 @@ public class WorldManager : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+
+    public void JoinOnline()
+    {
+        SceneManager.LoadScene("Loading");
+    }
 }
