9910ba9c0aa7c0c4cc154efad439cad58ff736dd
MassCon <53021665+MassCon@users.noreply.github.com>
2024-06-11 08:26:36 +0200

    Multiplayer lobby

--- Assets/Scripts/Multiplayer/ConnectToServer.cs
+++ Assets/Scripts/Multiplayer/ConnectToServer.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
+    public TMP_InputField usernameInput;
+    public TMP_Text buttonText;
+
     // Start is called before the first frame update
-    void Start()
+
+    public void OnClickConnect()
     {
-        PhotonNetwork.ConnectUsingSettings();
+        if(usernameInput.text.Length >= 1)
+        {
+            PhotonNetwork.NickName = usernameInput.text;
+            buttonText.text = "Connecting...";
+            PhotonNetwork.ConnectUsingSettings();
+        }
     }
 
-    // Update is called once per frame
-    public override void OnConnectedToMaster()
-    {
-        PhotonNetwork.JoinLobby();
-    }
+
 
     public override void OnJoinedLobby()
     {
