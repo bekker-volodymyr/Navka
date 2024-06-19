13ff3cf43ec9c91c1e99e3ccb0a1411e88c25b3a
MassCon <53021665+MassCon@users.noreply.github.com>
2024-06-19 15:24:36 +0200

    respawn fix

--- Assets/Scripts/UI/DeathScreen/DeathScreen.cs
+++ Assets/Scripts/UI/DeathScreen/DeathScreen.cs
@@ -1,24 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DeathScreen : MonoBehaviour
 {
-    [SerializeField] GameObject deathScreen;
+    GameObject deathScreen;
+    CinemachineVirtualCamera _camera;
+    private Player player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        deathScreen = GameObject.FindAnyObjectByType<DeathScreen>().gameObject;
+        _camera = GameObject.FindAnyObjectByType<CinemachineVirtualCamera>();
+    }
 
     public void RespawnClick()
     {
         deathScreen.SetActive(false);
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-
+       
         float randomX = Random.Range(-50, 50);
         float randomY = Random.Range(-50, 50);
 
         Vector2 newPosition = new Vector2(randomX, randomY);
         player.transform.position = newPosition;
+        player.Respawn();
 
-        GameManager.isPaused = false;
+        GameManager.isDead = false;
+
+        _camera.Follow = player.transform;
+        _camera.LookAt = player.transform;
+
+        Time.timeScale = 1f;
+
     }
 
     public void QuitClick()
