07da318b45a0527b3e97f454b7bb538c4c51ee26
YurkevichVA <bekker.volodymyr.yu@gmail.com>
2024-06-25 12:26:50 +0300

    new sprites + hideout tilemap rework

--- Assets/Scripts/Hideout/HideoutExit.cs
+++ Assets/Scripts/Hideout/HideoutExit.cs
@@ -1,17 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class HideoutExit : MonoBehaviour, IInteractable
 {
-    private void EnterHideout()
+    private void ExitHideout()
     {
         SceneManager.LoadScene(GameManager.worldScene);
     }
 
     public void OnInteraction(GameObject interactObject)
     {
-        EnterHideout();
+        ExitHideout();
     }
 }
