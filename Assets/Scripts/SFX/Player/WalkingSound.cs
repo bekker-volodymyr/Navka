fefdba8c83d5c2807a3a0a1f4462b84298149378
MassCon <53021665+MassCon@users.noreply.github.com>
2024-06-26 17:13:29 +0400

    walking sound final fix

--- Assets/Scripts/SFX/Player/WalkingSound.cs
+++ Assets/Scripts/SFX/Player/WalkingSound.cs
@@ -6,9 +6,10 @@ public class WalkingSound : MonoBehaviour
 {
     [SerializeField] private GameObject sound;
     [SerializeField] private Player player;
+    [SerializeField] private Rigidbody2D rb;
     [SerializeField] private List<string> keyNames;
 
-    private Vector3 lastPosition;
+    //private Vector3 lastPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -21,30 +22,49 @@ public class WalkingSound : MonoBehaviour
     void Update()
     {
 
-
-        foreach (string keys in keyNames)
-        {
-            if (Input.GetKeyDown(keys))
-            {
-                PlaySound();
-                //lastPosition = player.transform.position;
-            }
-            if (Input.GetKeyUp(keys))
-            {
-                StopSound();
-            }
-        }
-
-
-        if (player.StateMachine.CurrentState == player.MoveToPointState)
+        if (rb.velocity.magnitude > 0.1f)
         {
             PlaySound();
+            //// If the player is moving and the audio is not playing, play the audio
+            //if (!audioSource.isPlaying)
+            //{
+            //    audioSource.Play();
+            //}
         }
-        //if (player.StateMachine.CurrentState != player.MoveToPointState)
-        //{
-        //    StopSound();
-        //}
+        else
+        {
+            StopSound();
+            //// If the player is not moving and the audio is playing, stop the audio
+            //if (audioSource.isPlaying)
+            //{
+            //    audioSource.Stop();
+            //}
+        }
     }
+
+
+    //foreach (string keys in keyNames)
+    //{
+    //    if (Input.GetKeyDown(keys))
+    //    {
+    //        PlaySound();
+    //        //lastPosition = player.transform.position;
+    //    }
+    //    if (Input.GetKeyUp(keys))
+    //    {
+    //        StopSound();
+    //    }
+    //}
+
+
+    //if (player.StateMachine.CurrentState == player.MoveToPointState)
+    //{
+    //    PlaySound();
+    //}
+    //if (player.StateMachine.CurrentState != player.MoveToPointState)
+    //{
+    //    StopSound();
+    //}
 
     void PlaySound()
     {
@@ -56,3 +76,4 @@ public class WalkingSound : MonoBehaviour
         sound.SetActive(false);
     }
 }
+
