990f98968e2b84200a6c04a4ad4fcf2c8df4edbb
MassCon <53021665+MassCon@users.noreply.github.com>
2024-06-28 15:23:50 +0400

    day night cycle fixed + rendering

--- Assets/Scripts/VFX/WorldLight.cs
+++ Assets/Scripts/VFX/WorldLight.cs
@@ -8,15 +8,15 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Light2D))]
 public class WorldLight : MonoBehaviour
 {
-    private Light2D light;
+    private new Light2D light;
     [SerializeField] private WorldTime worldTime;
     [SerializeField] private Gradient gradient;
-    [SerializeField] private Image panelImage; 
+    //[SerializeField] private Image panelImage; 
 
     private void Awake()
     {
         light = GetComponent<Light2D>();
-        panelImage = GetComponent<Image>();
+        //panelImage = GetComponent<Image>();
         worldTime.WorldTimeChanged += OnWorldTimeChanged;
     }
 
@@ -28,7 +28,7 @@ public class WorldLight : MonoBehaviour
     private void OnWorldTimeChanged(object sender, TimeSpan newTime)
     {
         light.color = gradient.Evaluate(PercentOfTheDay(newTime));
-        panelImage.color = gradient.Evaluate(PercentOfTheDay(newTime));
+        //panelImage.color = gradient.Evaluate(PercentOfTheDay(newTime));
     }
 
     private float PercentOfTheDay(TimeSpan timeSpan)
